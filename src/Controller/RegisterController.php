a09f4481a21312c603e32ef2280aaf39fd98ae14
xsium <simon.metayerlr@gmail.com>
2025-02-24 18:03:18 +0100

    ajout recaptcha+css

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3Validator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+final class RegisterController extends AbstractController{
+    #[Route('/register/adduser', name: 'app_register')]
+    public function registerUser(Request $request, EntityManagerInterface $entityManager, Recaptcha3Validator $recaptcha3Validator,UrlGeneratorInterface $urlGenerator): Response
+    {
+         // Create user entity
+         $user = new User();
+
+         // Create form
+         $form = $this->createForm(RegisterType::class, $user);
+ 
+         // Handle request
+         $form->handleRequest($request);
+ 
+         // Initialize message variable
+ 
+         if ($form->isSubmitted() && $form->isValid()) {
+            // Get score
+            $recaptchaResponse = $recaptcha3Validator->getLastResponse();
+            $score = $recaptchaResponse ? $recaptchaResponse->getScore() : 0;
+            // Check if user is a bot
+            if ($score < 0.5) {
+                $this->addFlash('danger', "L'utilisateur est un bot"); //utilisation de addflash pour éviter l'erreur de turbodriver
+                return $this->redirectToRoute('app_register');
+            } else {
+                $entityManager->persist($user);
+                $entityManager->flush();
+        
+                $this->addFlash('success', "Registration successful!");
+                return new RedirectResponse($urlGenerator->generate('app_register'));
+            }
+        }
+ 
+         return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
+use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+            ->add('save', SubmitType::class)
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => [new Recaptcha3()],
+                'action_name' => 'register/adduser',
+                'locale' => 'fr',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
